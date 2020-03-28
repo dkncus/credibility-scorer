616c81e075dd1994cf67dd6fe6cf4a94a63dbb50
dkncus <dkncus@gmail.com>
2020-03-28 11:05:38 -0400

    Initial Commit

---
+++ Documentation/Coding Standards/header.py
@@ -0,0 +1,13 @@
+'''
+Name : <Name of Script>
+Date : <Date of Module Creation>
+Author: <Original Author, First and Last>
+Modification History: - <List of times modified/author>
+(e.g. “John Doe, 12 MAR 2020”)
+Synopsis: <Brief description of what this code does>
+Methods: <List of methods supported in the module>
+	# e.g. “getX() – Returns x Variable”
+	# e.g. “setY(y_set) – Sets the Y variable”
+Global Variables: <List of global variables modified>
+'''
+hello = 5
