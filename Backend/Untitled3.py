c61a8da4cc642a7964505802a076e3438a231cba
Tiger <62891682+xyztiger@users.noreply.github.com>
2020-04-04 19:13:50 -0700

    is_it_real_news_nltk_preprocessing

---
+++ Backend/Untitled3.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import nltk, csv, random
+
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+stop_words = set(stopwords.words("english"))
+lemmatizer = WordNetLemmatizer()
+
+ListOfNews = []
+
+with open("Fake.csv") as csvfile:
+        
+    reader = csv.reader(csvfile)
+    next(reader) # skip header line
+
+    for row in reader:
+        title = word_tokenize(row[0])
+        body = []
+        for w in word_tokenize(row[1]):
+            if w not in stop_words:
+                wLower = w.lower()
+                wLowerLematized = lemmatizer.lemmatize(wLower)
+                body.append(wLowerLematized)
+        real = False
+        
+        news = (title, body, real)
+        
+        ListOfNews.append(news)
+        
+with open("True.csv") as csvfile:
+        
+    reader = csv.reader(csvfile)
+    next(reader) # skip header line
+
+    for row in reader:
+        title = word_tokenize(row[0])
+        body = []
+        for w in word_tokenize(row[1]):
+            if w not in stop_words:
+                wLower = w.lower()
+                wLowerLematized = lemmatizer.lemmatize(wLower)
+                body.append(wLowerLematized)
+        real = False
+        
+        news = (title, body, real)
+        
+        ListOfNews.append(news)
+        
+random.shuffle(ListOfNews)
+
+print(ListOfNews[0])
+
